60ecaa5034ee0efc7f7556520f4ac5393aabea73
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-14 18:11:59 +0000

    creating array added

---
+++ app.js
@@ -0,0 +1,12 @@
+//creating arrays
+const numbers = [1, 2, 3];
+const moreNumber = new Array();
+const evenMoreNumbers = new Array(5);
+const yetMoreNumbers = Array.of(1, 2, 3);
+const convertedArray = Array.from('hi!');
+
+console.log(numbers);
+console.log(moreNumber);
+console.log(evenMoreNumbers);
+console.log(yetMoreNumbers);
+console.log(convertedArray);
